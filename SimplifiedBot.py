701405280387fa2780a4bf3206e22085ca866161
Yonah Mann <yonahmann@mail.tau.ac.il>
2025-06-18 21:00:18 +0300

    Added simplified bot

---
+++ SimplifiedBot.py
@@ -0,0 +1,24 @@
+import arena_globals
+from bot import Bot
+
+
+class SimplifiedBot(Bot):
+    def move_right(self):
+        self.moveX = 1
+        self.moveY = 0
+        self.action = self.ACTION_MOVE
+
+    def move_left(self):
+        self.moveX = -1
+        self.moveY = 0
+        self.action = self.ACTION_MOVE
+
+    def move_up(self):
+        self.moveX = 0
+        self.moveY = 1
+        self.action = self.ACTION_MOVE
+
+    def move_down(self):
+        self.moveX = 0
+        self.moveY = -1
+        self.action = self.ACTION_MOVE
